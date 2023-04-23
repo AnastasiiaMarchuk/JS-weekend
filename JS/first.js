2fdc1928d878ed8f8619d1069c7ee86355805c1e
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-04-23 10:11:37 +0200

    1

--- JS/first.js
+++ JS/first.js
@@ -22,3 +22,16 @@
 // const phonePrise = Number(prompt("Ціна телефону"));
 // const blenderPrise = Number(prompt("Ціна блендера"));
 // console.log(computerPrise + phonePrise + blenderPrise);
+
+
+// ЗАДАЧА 4
+// ВИКОРИСТОВУЮЧИ КОНСТРУКЦІЮ IF...ELSE , НАПИШИ КОД, ЯКИЙ БУДЕ ЗАПИТУВАТИ: 
+// "СКІЛЬКИ МІСЯЦІВ МАЄ РІК", ЯКЩО КОРИСТУВАЧ ВВОДИТЬ 12 ТО У КОНСОЛІ ПИШЕ ВІРНО, ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ 
+// "НЕ ЗНАЄТЕ? 12"
+
+// const year = Number(prompt('how years'))
+// if (year === 12) {
+//     console.log(right)
+// } else {
+//     console.log("НЕ ЗНАЄТЕ? 12")
+// }
