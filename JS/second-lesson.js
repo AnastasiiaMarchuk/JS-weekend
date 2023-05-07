b9b8e80ca69cd45bdc98102016adb202695772d0
AnastasiaCarlii <nastjaorosszul@gmail.com>
2023-05-07 09:42:21 +0100

    Update second-lesson.js

--- JS/second-lesson.js
+++ JS/second-lesson.js
@@ -57,9 +57,26 @@
 // }
 // ЗАДАЧА 4
 //  Провірте , чи являється рядок полідромом
-const str = "ротатор"; //true
-const newString = "Hello"; //false
-const world = str.split("").reverse().join("");
-console.log(world === str);
-const string = newString.split("").reverse().join("");
-console.log(string === newString);
+// const str = "ротатор"; //true
+// const newString = "Hello"; //false
+// const world = str.split("").reverse().join("");
+// console.log(world === str);
+// const string = newString.split("").reverse().join("");
+// console.log(string === newString);
+
+// ЗАДАЧА 5
+// Напишіть ф-цію calculateAverage() яка приймає довільну кількість аргументів і повертає їх середнє значення .Всі аргументи будуть тільки числовими
+// function calculateAverage() {
+//   console.log(arguments);
+//   const numbers = Array.from(arguments);
+
+//   let total = 0;
+//   for (let number of numbers) {
+//     total += number;
+//   }
+//   return total / numbers.length;
+// }
+
+// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calculateAverage(14, 8, 2)); // 8
+// console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
