4a35d18c26fc7b8f87399f5d280ec21073ab9e46
Mario031281 <taracatus@gmail.com>
2023-05-07 12:38:32 +0300

    yg

--- JS/second-lesson.js
+++ JS/second-lesson.js
@@ -81,20 +81,16 @@
 // console.log(calculateAverage(14, 8, 2)); // 8
 // console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
 
-
-
 // ЗАДАЧА 6
-// Напишіть ф-цію greet(name) , яка при виклику буде получати імя (як приклад , "Василь"), і логінувати рядок "Привіт, <імя>".У випадку , відсутнього 
+// Напишіть ф-цію greet(name) , яка при виклику буде получати імя (як приклад , "Василь"), і логінувати рядок "Привіт, <імя>".У випадку , відсутнього
 // відсутнього значення, виводе аргумент "Привіт , гість"
 
 // function greet(name = "гість") {
-    
+
 //     return `Привіт, ${name}`;
 // }
 //  console.log(greet("Василь"));//Привіт Василь
 //  console.log(greet());//Привіт гість
-
-
 
 // ЗАДАЧА 7
 // Виконайте рефакторинг замінивши обявлення ф-ції з function declaration на function expression та на стрілочну функцію
@@ -106,12 +102,11 @@
 //   return `число ${b} більше ${a}`;
 // }
 
-
 // const foo = function checkNumbers(a, b) {
 //       if (a > b) {
 //         return `число ${a} більше ${b}`;
 //       }
-    
+
 //       return `число ${b} більше ${a}`;
 //     }
 
@@ -119,18 +114,15 @@
 //     if (a > b) {
 //         return `число ${a} більше ${b}`;
 //       }
-    
+
 //       return `число ${b} більше ${a}`;
 // }
 
 // const checkNumbers = (a, b) => a > b ? `число ${a} більше ${b}` :  `число ${b} більше ${a}`
 
-
-
 // // ЗАДАЧА 8
 // // Напишіть ф-цію capitalize , яка буде приймати рядок і буде повертати новий рядок , де кожне слово буде з великої букви
 // console.log(capitalize('the quick brown fox'))// 'The Quick Brown Fox '
-
 
 // function capitalize(string) {
 //     const words = string.split(' ');
@@ -143,3 +135,37 @@
 //     }
 //     return ajax.join(' ');
 // }
+// ЗАДАЧА 9
+// Напишіть функцію для роботи з колекцією навчаючих курсів courses:
+
+// addCourse(name) - добавляє курс у кінець колекції
+// removeCourse(name) - видаляє курс із колекції
+// updateCourse(oldName, newName)- змінює імя на нове
+const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+
+function addCourse(course) {
+  if (courses.includes(course)) {
+    console.log("У вас уже є такий курс");
+    return;
+  }
+  courses.push(course);
+}
+function removeCourse(course) {
+  let courseIndex = courses.indexOf(course);
+  if (courseIndex === -1) {
+    console.log("Курс з таким іменем не найдено");
+    return;
+  }
+  courses.splice(courseIndex, 1);
+}
+
+function updateCourse(courseToDelete, courseToAdd) {
+  let oldName = courses.indexOf(courseToDelete);
+  courses.splice(oldName, 1, courseToAdd);
+}
+addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+addCourse("CSS"); // 'У вас уже є такий курс'
+removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+removeCourse("Vue"); // 'Курс з таким іменем не найдено'
+updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+console.log(courses);
