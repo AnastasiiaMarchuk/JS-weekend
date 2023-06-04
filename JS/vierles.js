2f719f8421834c7e7939b2dbaae78fa0915e1bfb
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-06-04 11:17:41 +0200

    artur

--- JS/vierles.js
+++ JS/vierles.js
@@ -33,29 +33,77 @@
 // Створіть карточки товару з масиву використовуючи createElement
 
 // js/
-import cars from './date/cars.js'
+// import cars from './date/cars.js'
 
 
-const carsList = document.querySelector('.js-goods');
+// const carsList = document.querySelector('.js-goods');
 
-function makeProductCards ({name, img, price, description}){
-  const element = document.createElement('li');
-  const elementName = document.createElement('h2');
-  elementName.textContent = name;
-  const elementImg = document.createElement('img');
-  elementImg.src = img;
-  elementImg.width = 200;
-  const elementPrice = document.createElement('p');
-  elementPrice.textContent = `Price ${price} грн`;
-  elementPrice.classList.add('class-ul');
-  const elementDescription = document.createElement('p');
-  elementDescription.textContent = description;
+// function makeProductCards ({name, img, price, description}){
+//   const element = document.createElement('li');
+//   const elementName = document.createElement('h2');
+//   elementName.textContent = name;
+//   const elementImg = document.createElement('img');
+//   elementImg.src = img;
+//   elementImg.width = 200;
+//   const elementPrice = document.createElement('p');
+//   elementPrice.textContent = `Price ${price} грн`;
+//   elementPrice.classList.add('class-ul');
+//   const elementDescription = document.createElement('p');
+//   elementDescription.textContent = description;
 
-  element.append(elementImg, elementName, elementPrice, elementDescription);
-  return element;
-}
+//   element.append(elementImg, elementName, elementPrice, elementDescription);
+//   return element;
+// }
 
-const markup = cars.map(makeProductCards);
-console.log(markup);
-carsList.append(...markup);
+// const markup = cars.map(makeProductCards);
+// console.log(markup);
+// carsList.append(...markup);
 
+// ЗАДАЧА 2
+// 2.1НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА КІЛОГРАМ І КІЛЬКОСТІ КУПЛЕНОГО ПРОДУКТА , І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
+// html/
+
+// const refs = {
+//   form: document.querySelector('.form'),
+//   formInput: document.querySelector('#price'),
+//   inputRange: document.querySelector('#quantity'),
+//   totalPrice: document.querySelector('.total'),
+//   label: document.querySelector('.amount'),
+//   container: document.querySelector('.container')
+// }
+    
+// function calculator(event) {
+//   event.preventDefault();
+//   const figuresInput = refs.formInput.value;
+//   const figuresRange = refs.inputRange.value;
+//   const total = figuresInput * figuresRange;
+//   refs.totalPrice.textContent = total;
+//   refs.label.textContent = figuresRange;
+//   return total;
+// }
+
+// window.addEventListener("DOMContentLoaded", calculator);
+
+// refs.form.addEventListener('submit', calculator);
+
+// // 2.2 ПЕРЕПИШІТЬ КОД, ЩОБ ЗНАЧЕННЯ МИ ОТРИМУВАЛИ ПО КЛІЦІ НА КНОПКУ,
+// //   ПРИ НАЖАТТІ ЯКОГО БУДЕ ВАМ РАХУВАТИ ТА ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
+// //     < button class="btn" > Calculate</ >
+
+// function onButtonClick() {
+//   const figuresRange = refs.inputRange.value;
+//    refs.label.textContent = figuresRange;
+// }
+
+// refs.inputRange.addEventListener('input', onButtonClick);
+
+// // 2.3 НАПИШІТЬ  РАНДОМНИЙ КОЛІР ДЛЯ НАШОЇ ФОРМИ КОЛЬКУЛЯТОРА ЗА ДОПОМОГОЮ 
+// // ІНЛАЙНОВОГО СТИЛЮ
+// const randomRgbColor = () => {
+//   const r = Math.round(Math.random() * (255 - 1) + 1);
+//   const g = Math.round(Math.random() * (255 - 1) + 1);
+//   const b = Math.round(Math.random() * (255 - 1) + 1);
+//   return `rgb(${r},${g},${b})`;
+// };
+
+// refs.container.style.backgroundColor = randomRgbColor();
